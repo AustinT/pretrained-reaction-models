29c6a2455c671acf8c77ca4b96d42100d882e84b
Austin T <austin.james.tripp@gmail.com>
2023-05-02 13:11:34 +0000

    Fix errors in download_files script.

--- retro_star_task/download_files.sh
+++ retro_star_task/download_files.sh
@@ -1,8 +1,9 @@
 # Script to download retro-star files
 file_name="retro_data.zip"
 wget https://www.dropbox.com/s/ar9cupb18hv96gj/retro_data.zip?dl=0 -O ./files/$file_name
+cd ./files
 unzip $file_name
 
 # Create canonical origin dict
-cd files/dataset
+cd dataset
 python canonicalize_origin_dict.py
